8d2a891c7cec89beb21c1aaa2767b2bf07e1d56f
Seth Kunzler <sethkunzler@gmail.com>
2024-02-16 11:29:50 -0700

    first commit

---
+++ app/router-config.js
@@ -0,0 +1,26 @@
+import { AccountController } from "./controllers/AccountController.js";
+import { AuthGuard } from "./services/AuthService.js";
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [],
+    view: `app/views/HomeView.html`
+  },
+  {
+    path: '#/about',
+    view: 'app/views/AboutView.html'
+  },
+  {
+    path: '#/account',
+    middleware: [AuthGuard],
+    controllers: [AccountController],
+    view: 'app/views/AccountView.html',
+  }
+])
+
+
+
+
